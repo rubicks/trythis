dadeb1ec96d8799650895e88006f471b0c099b81
Your Name <you@example.com>
2015-05-27 13:28:42 -0400

    adds ex6.sql

---
+++ sqlite/ex6.sql
@@ -0,0 +1,12 @@
+-- sqlite/ex6.sql
+
+.read "ex4.sql"
+
+select pet.id, pet.name, pet.age, pet.dead
+  from pet, person_pet, person
+  where
+    pet.id = person_pet.pet_id and
+    person_pet.person_id = person.id and
+    person.first_name = "Zed"
+    ;
+
